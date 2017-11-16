7bc3af0a56ebc2acf46cd9df7373116293e212cd
SHEFFmachine <sheff@sheffmachine.com>
2017-11-15 22:09:16 -0800

    Quicksort

---
+++ Quicksort.py
@@ -0,0 +1,19 @@
+class Quicksort:
+    def quicksort_wrapper(self, a, start, end):
+        if start < end:
+            partition_index = self.partition(a, start, end)
+            self.quicksort_wrapper(a, start, partition_index - 1)
+            self.quicksort_wrapper(a, partition_index + 1, end)
+
+    @staticmethod
+    def partition(self, a, start, end):
+        pivot = a[end]
+        i = start - 1
+        for j in range(start, end, 1):
+            if a[j] < pivot:
+                i += 1
+                a[j], a[i] = a[i], a[j]
+
+        a[i + 1], a[end] = a[end], a[i + 1]
+
+        return i + 1
