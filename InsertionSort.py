24c295c4c9a7e53209dce7480c5c16e247e5511f
SHEFFmachine <sheff@sheffmachine.com>
2017-11-15 20:12:57 -0800

    insertion sort

---
+++ InsertionSort.py
@@ -0,0 +1,14 @@
+class InsertionSort:
+    def run(inputArr):
+        length = len(inputArr)
+        for i in range(0, length, 1):
+            key = inputArr[i]
+            j = i - 1
+            while j >= 0 and key < inputArr[j]:
+                inputArr[j + 1] = inputArr[j]
+                j -= 1
+
+            inputArr[j + 1] = key
+
+        for item in inputArr:
+            print item
