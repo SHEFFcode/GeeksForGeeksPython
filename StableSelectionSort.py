45e12e09cfa976dbde0199b89b68d3b60c9670c7
SHEFFmachine <sheff@sheffmachine.com>
2017-11-20 19:40:50 -0800

    Clean up stable selection sort

--- StableSelectionSort.py
+++ StableSelectionSort.py
@@ -1,21 +1,19 @@
 class StableSelctionSort:
     def run(self, input_arr):
-        length = len(input_arr)
+        length = len(input_arr)  # Take hold of the input_arr length
 
-        for i in range(0, length - 1, 1):
-            lowest_item_index = i
+        for i in range(0, length - 1):  # iterate over the input_arr
+            lowest_item_index = i  # assume lowest index is current index
 
-            for j in range(i, length, 1):
-                if input_arr[j] < input_arr[lowest_item_index]:
-                    lowest_item_index = j
+            for j in range(i, length):  # iterate over the input_arr again from i to length on every i loop above
+                if input_arr[j] < input_arr[lowest_item_index]:  # if the subsequent to i value is lower then one @ i
+                    lowest_item_index = j  # lowest item index becomes the value at j
 
-            key = input_arr[lowest_item_index]
-            while lowest_item_index > i:
+            key = input_arr[lowest_item_index]  # the value of the item at lowest_item_index
+            while lowest_item_index > i:  # here we are moving the items to the right to free up some space
                 input_arr[lowest_item_index] = input_arr[lowest_item_index - 1]
                 lowest_item_index -= 1
-            input_arr[i] = key
+            input_arr[i] = key  # we put the value of lowest_item_index into that spot in the array
 
         for item in input_arr:
             print item
-
-
