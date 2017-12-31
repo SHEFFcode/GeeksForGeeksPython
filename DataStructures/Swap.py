63dcf759fe71096e90b335440b078bb4d1d8aa6c
SHEFFmachine <sheff@sheffmachine.com>
2017-12-31 10:00:30 -0800

    swap

---
+++ DataStructures/Swap.py
@@ -0,0 +1,4 @@
+class Swap:
+    @staticmethod
+    def swap(int_one, int_two):
+        int_one, int_two = int_two, int_one  # no rhyme or reason for this, just to remember how once line swap works
